847a253e32f449b727595f2e0ec2e1a018a76e4b
Fabian Gutierrez <fabiangutierrez580@gmail.com>
2022-09-12 09:35:15 -0500

    time blocks all colored correctly (:

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ $(".saveBtn").on("click", function(){
     console.log(text);
 })
 
-$("#hour9").text(localStorage.getItem("9"))
+$("#hour09").text(localStorage.getItem("09"))
 $("#hour10").text(localStorage.getItem("10"))
 $("#hour11").text(localStorage.getItem("11"))
 $("#hour12").text(localStorage.getItem("12"))
@@ -26,28 +26,38 @@ $("#hour15").text(localStorage.getItem("15"))
 $("#hour16").text(localStorage.getItem("16"))
 $("#hour17").text(localStorage.getItem("17"))
 
-// function colorChanger(){
-//     let currentHour = moment().hour()
-//    if(currentHour > )
-// }colorChanger()
+
+let currentHour = moment().hour();
+console.log(currentHour)
+
+
 
 $(".longBlock").each(function() {
-    let blockHour = parseInt($(this).attr("id"));
-    console.log (this);
+let timeBlockHour=parseInt($(this).attr("id"));
+console.log(timeBlockHour);
+if (timeBlockHour === currentHour) {
+   // $("textarea").addclass(present);
+   //console.log(this.children[1])
+   $(this.children[1]).addClass("present");
+}
+else if (timeBlockHour > currentHour){
+    $(this.children[1]).addClass("future");
+}else{
+    $(this.children[1]).addClass("past");
+}
 });
-// longBlock();
 
-
-//   GIVEN I am using a daily planner to create a schedule
-// WHEN I open the planner
-// THEN the current day is displayed at the top of the calendar
-// WHEN I scroll down
-// THEN I am presented with time blocks for standard business hours
-// WHEN I view the time blocks for that day
-// THEN each time block is color-coded to indicate whether it is in the past, present, or future
-// WHEN I click into a time block
-// THEN I can enter an event
-// WHEN I click the save button for that time block
-// THEN the text for that event is saved in local storage
-// WHEN I refresh the page
-// THEN the saved events persist
+    
+    // //   GIVEN I am using a daily planner to create a schedule
+    // WHEN I open the planner
+    // THEN the current day is displayed at the top of the calendar
+    // WHEN I scroll down
+    // THEN I am presented with time blocks for standard business hours
+    // WHEN I view the time blocks for that day
+    // THEN each time block is color-coded to indicate whether it is in the past, present, or future
+    // WHEN I click into a time block
+    // THEN I can enter an event
+    // WHEN I click the save button for that time block
+    // THEN the text for that event is saved in local storage
+    // WHEN I refresh the page
+    // THEN the saved events persist
